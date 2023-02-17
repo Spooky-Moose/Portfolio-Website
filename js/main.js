2d2b385106dfc16423f8ff94cd2dd70e4f5d4190
Preston Valles <63984271+Spooky-Moose@users.noreply.github.com>
2023-02-16 23:37:12 -0700

    modal title updating

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ document.addEventListener("click", function (e){
     if(e.target.classList.contains("gallery-item")){
         const src = e.target.getAttribute("src");
         document.querySelector(".modal-img").src = src;
+        
+        const alt = e.target.getAttribute("alt");
+        document.querySelector(".modal-title").textContent = alt;
         //const myModal = new boostrap.Modal(document.getElementById('gallery-modal'));
         //myModal.show();
         $('#gallery-modal').modal('show')
