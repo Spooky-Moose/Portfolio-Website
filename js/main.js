17f93b5fa9dc1a629b7d01f9394c5a71e725f25a
Preston Valles <63984271+Spooky-Moose@users.noreply.github.com>
2023-02-16 23:20:19 -0700

    modal images

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener("click", function (e){
+    //console.log(e.target);
+    if(e.target.classList.contains("gallery-item")){
+        const src = e.target.getAttribute("src");
+        document.querySelector(".modal-img").src = src;
+        //const myModal = new boostrap.Modal(document.getElementById('gallery-modal'));
+        //myModal.show();
+        $('#gallery-modal').modal('show')
+    }
+})
