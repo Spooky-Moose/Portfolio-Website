12da014e28cf281f0c95edd4230b782937ac4e14
Preston Valles <63984271+Spooky-Moose@users.noreply.github.com>
2023-02-22 22:21:41 -0700

    Recent changes

--- js/photo.js
+++ js/photo.js
@@ -1,20 +1,20 @@
 //Right Click Disabled for Gallery Items
 document.addEventListener('contextmenu', (e) => {
-    
-    if (e.target.classList.contains("gallery-item") || ("modal-img")){
+
+    if (e.target.classList.contains("gallery-item") || ("modal-img")) {
         e.preventDefault();
     }
-    
+
 
 });
 
 //Modal View Event Listener
-document.addEventListener("click", function (e){
+document.addEventListener("click", function (e) {
     //console.log(e.target);
-    if(e.target.classList.contains("gallery-item")){
+    if (e.target.classList.contains("gallery-item")) {
         const src = e.target.getAttribute("src");
         document.querySelector(".modal-img").src = src;
-        
+
         const alt = e.target.getAttribute("alt");
         document.querySelector(".modal-title").textContent = alt;
         //const myModal = new boostrap.Modal(document.getElementById('gallery-modal'));
