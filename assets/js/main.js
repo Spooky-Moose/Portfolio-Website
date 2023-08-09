34b8e3188f6171e11e4627cb0d7302909aac15a8
Preston Valles <63984271+Spooky-Moose@users.noreply.github.com>
2023-08-09 14:50:30 -0600

    IMG Gallery modals functioning

--- assets/js/main.js
+++ assets/js/main.js
@@ -246,27 +246,27 @@
 
 })()
 
-// //Right Click Disabled for Gallery Items
-// document.addEventListener('contextmenu', (e) => {
+//Right Click Disabled for Gallery Items
+document.addEventListener('contextmenu', (e) => {
 
-//   if (e.target.classList.contains("gallery-item") || ("modal-img")) {
-//       e.preventDefault();
-//   }
+  if (e.target.classList.contains("gallery-item") || ("modal-img")) {
+      e.preventDefault();
+  }
 
 
-// });
+});
 
-// //Modal View Event Listener
-// document.addEventListener("click", function (e) {
-//   //console.log(e.target);
-//   if (e.target.classList.contains("gallery-item")) {
-//       const src = e.target.getAttribute("src");
-//       document.querySelector(".modal-img").src = src;
+//Modal View Event Listener
+document.addEventListener("click", function (e) {
+  //console.log(e.target);
+  if (e.target.classList.contains("gallery-item")) {
+      const src = e.target.getAttribute("src");
+      document.querySelector(".modal-img").src = src;
 
-//       const alt = e.target.getAttribute("alt");
-//       document.querySelector(".modal-title").textContent = alt;
-//       //const myModal = new boostrap.Modal(document.getElementById('gallery-modal'));
-//       //myModal.show();
-//       $('#gallery-modal').modal('show')
-//   }
-// })
+      const alt = e.target.getAttribute("alt");
+      document.querySelector(".modal-title").textContent = alt;
+      //const myModal = new boostrap.Modal(document.getElementById('gallery-modal'));
+      //myModal.show();
+      $('#gallery-modal').modal('show')
+  }
+})
